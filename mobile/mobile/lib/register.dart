1b9d7fdc43fb43e1daf8eecc8b8f73838e9ab757
김태현 <bear2u@naver.com>
2019-07-29 21:45:00 +0900

    update

--- mobile/mobile/lib/register.dart
+++ mobile/mobile/lib/register.dart
@@ -9,27 +9,66 @@ class Register extends StatefulWidget {
 }
 
 class RegisterState extends State<Register> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _id,_pwd;
+
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(),
-          TextField(),
-          Row(
-            children: <Widget>[
-              RaisedButton(
-                child: Text('finish'),
-                onPressed: () {
-
-                },
-              )
-            ],
-          )
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                print('1st $value');
+                if(value.isEmpty) {
+                  return 'check id';
+                }                
+              },
+              onSaved: (value) {
+                setState(() {
+                  _id = value;
+                });                
+              },
+            ),
+            TextFormField(
+              validator: (value) {
+                print('2st $value');
+                if(value.isEmpty) {
+                  return 'check pwd';
+                }                
+              },
+              obscureText: true,
+              onSaved: (value) {
+                setState(() {
+                  _pwd = value;
+                });           
+              },
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('finish'),
+                  onPressed: () {
+                    final _form = _formKey.currentState;
+                    if(_form.validate()) {
+                      // TODO 유효성 체크 완료
+                      _form.save();
+                      print('유효성 체크 통과 , $_id, $_pwd');
+                    } else {
+                      print('유효성 체크 실패');
+                    }
+                  },
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
